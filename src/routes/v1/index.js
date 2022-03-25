6c3c7552d62ce4f9ee183f7cdb49e1d9dbf93e11
HaiDang309 <hada.nguyen309@gmail.com>
2022-03-25 15:08:44 +0700

    .

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,7 +4,7 @@ const userRoute = require('./user.route');
 const postRoute = require('./post.route');
 const commentRoute = require('./comment.route');
 const docsRoute = require('./docs.route');
-const config = require('../../config/config');
+// const config = require('../../config/config');
 
 const router = express.Router();
 
@@ -25,25 +25,29 @@ const defaultRoutes = [
     path: '/comments',
     route: commentRoute,
   },
-];
-
-const devRoutes = [
-  // routes available only in development mode
   {
     path: '/docs',
     route: docsRoute,
   },
 ];
 
+// const devRoutes = [
+//   // routes available only in development mode
+//   {
+//     path: '/docs',
+//     route: docsRoute,
+//   },
+// ];
+
 defaultRoutes.forEach((route) => {
   router.use(route.path, route.route);
 });
 
 /* istanbul ignore next */
-if (config.env === 'development') {
-  devRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-  });
-}
+// if (config.env === 'development') {
+//   devRoutes.forEach((route) => {
+//     router.use(route.path, route.route);
+//   });
+// }
 
 module.exports = router;
