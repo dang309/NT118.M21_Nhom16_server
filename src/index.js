f433bb7486f88582e5e079c17a6902861cf5cd46
HaiDang309 <hada.nguyen309@gmail.com>
2022-03-25 14:01:56 +0700

    fix: change port

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const logger = require('./config/logger');
 // });
 
 const server = app.listen(config.port, () => {
-  logger.info(`Listening to port ${process.env.PORT || config.port}`);
+  logger.info(`Listening to port ${process.env.PORT || 5000}`);
 });
 
 const exitHandler = () => {
