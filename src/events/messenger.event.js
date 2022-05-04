4ec17dec3dfb7b042329c5f240489f9f32a0d89e
HaiDang309 <hada.nguyen309@gmail.com>
2022-05-04 23:56:08 +0700

    .

--- src/events/messenger.event.js
+++ src/events/messenger.event.js
@@ -3,7 +3,16 @@ const { Message } = require('../models');
 module.exports = (io, socket) => {
   const sendPrivateMessages = async (payload) => {
     const { messageId, content, from, to } = payload;
-    const newMessage = await Message.create({ message_id: messageId, content, from, to });
+    const _contactIds = [];
+    _contactIds.push(from);
+    _contactIds.push(to);
+    const newMessage = await Message.create({
+      message_id: messageId,
+      contact_id: _contactIds.sort().join('_'),
+      content,
+      from,
+      to,
+    });
     socket.to(to).emit('messenger:send_private_message', newMessage);
   };
 
